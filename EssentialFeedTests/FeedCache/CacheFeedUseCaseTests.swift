d6b4e9c182d5ff2b48f09986daa12908892c5c1f
Rattan <rattan@cqlsys.co.uk>
2023-12-17 15:05:20 +0530

    save commant does not request insertion on deletion error

--- EssentialFeedTests/FeedCache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/FeedCache/CacheFeedUseCaseTests.swift
@@ -24,9 +24,14 @@ class LocalFeedLoader {
 
 class FeedStore {
     var deleteCachedFeedCallCount  = 0
+    var insertCallCount = 0
     
     func deleteCachedFeed() {
         deleteCachedFeedCallCount += 1
+    }
+    
+    func completeDeletion(with error : Error, at index : Int = 0 ) {
+        
     }
 }
 
@@ -57,6 +62,17 @@ final class CacheFeedUseCaseTests: XCTestCase {
         XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
     }
 
+    func test_save_doesNotRequestCacheInsertionOnDeletionError() {
+        let items = [uniqueItem() , uniqueItem()]
+        let (sut , store) = makeSUT()
+        let deletionError = anyNSError()
+        
+        sut.save(items)
+        store.completeDeletion(with : deletionError)
+
+        
+        XCTAssertEqual(store.insertCallCount, 0)
+    }
     
     private func makeSUT(file: StaticString = #file,
                          line: UInt = #line) -> (sut : LocalFeedLoader, store : FeedStore) {
@@ -75,5 +91,10 @@ final class CacheFeedUseCaseTests: XCTestCase {
         
         return URL(string: "https://any-url.com")!
     }
+    
+    private func anyNSError() -> NSError {
+        return NSError(domain: "any error", code: 0)
+    }
+    
      
 }
