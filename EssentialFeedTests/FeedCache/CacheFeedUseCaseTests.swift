d293e4d48bb354e0e423358afb08353755ed673f
Rattan <rattan@cqlsys.co.uk>
2023-12-17 15:16:33 +0530

    save command request new cache insertion on successful deleteion

--- EssentialFeedTests/FeedCache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/FeedCache/CacheFeedUseCaseTests.swift
@@ -18,20 +18,38 @@ class LocalFeedLoader {
     }
     
     func save(_ items : [FeedItem] ) {
-        store.deleteCachedFeed()
+        store.deleteCachedFeed {[unowned self] error in
+            if error == nil {
+                self.store.insert(items)
+            }
+        }
     }
 }
 
 class FeedStore {
+    
+    typealias DeletionCompletion = (Error?) -> Void
+    
     var deleteCachedFeedCallCount  = 0
     var insertCallCount = 0
     
-    func deleteCachedFeed() {
+    private var deletionCompletion =  [DeletionCompletion]()
+    
+    func deleteCachedFeed(completion : @escaping DeletionCompletion ) {
         deleteCachedFeedCallCount += 1
+        deletionCompletion.append(completion)
     }
     
     func completeDeletion(with error : Error, at index : Int = 0 ) {
-        
+        deletionCompletion[index](error)
+    }
+    
+    func completeDeletionSuccessfully(at index : Int = 0 ) {
+        deletionCompletion[index](nil)
+    }
+    
+    func insert(_ items : [FeedItem] ) {
+        insertCallCount += 1
     }
 }
 
@@ -72,6 +90,18 @@ final class CacheFeedUseCaseTests: XCTestCase {
 
         
         XCTAssertEqual(store.insertCallCount, 0)
+    }
+    
+    func test_save_requestNewCacheInsetionOnSuccessfulDeletion() {
+        let items = [uniqueItem() , uniqueItem()]
+        let (sut , store) = makeSUT()
+        let deletionError = anyNSError()
+        
+        sut.save(items)
+        store.completeDeletionSuccessfully()
+
+        
+        XCTAssertEqual(store.insertCallCount, 1)
     }
     
     private func makeSUT(file: StaticString = #file,
