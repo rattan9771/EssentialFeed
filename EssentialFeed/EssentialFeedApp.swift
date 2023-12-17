dcf85ac424adb5d83a86dba74d4483d8d9d23cb2
Rattan <rattan@cqlsys.co.uk>
2023-12-17 13:45:48 +0530

    Initial Commit

---
+++ EssentialFeed/EssentialFeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  EssentialFeedApp.swift
+//  EssentialFeed
+//
+//  Created by Rattan Das on 17/12/23.
+//
+
+import SwiftUI
+
+@main
+struct EssentialFeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
