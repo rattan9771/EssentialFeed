11f2ff96fabce788421147fddae3b11fae9e33bb
Rattan <rattan@cqlsys.co.uk>
2023-12-17 14:51:49 +0530

    save command request cache deletion

--- EssentialFeed/FeedCache/CacheFeedUseCaseTests.swift
+++ EssentialFeed/FeedCache/CacheFeedUseCaseTests.swift
@@ -6,16 +6,31 @@
 //
 
 import XCTest
+import EssentialFeed
+
+
+class LocalFeedLoader {
+    
+    let store : FeedStore
+    
+    init(store : FeedStore) {
+        self.store = store
+    }
+    
+    func save(_ items : [FeedItem] ) {
+        store.deleteCachedFeed()
+    }
+}
 
 class FeedStore {
     var deleteCachedFeedCallCount  = 0
-}
-
-class LocalFeedStore {
-    init(store : FeedStore) {
-        
+    
+    func deleteCachedFeed() {
+        deleteCachedFeedCallCount += 1
     }
 }
+
+
 
 final class CacheFeedUseCaseTests: XCTestCase {
 
@@ -25,18 +40,32 @@ final class CacheFeedUseCaseTests: XCTestCase {
 
     func testExample() throws {}
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+   
     
     func test_init_doesNotDeleteCacheUponCreation() {
         
         let store = FeedStore()
-        _ = LocalFeedStore(store: store)
+        _ = LocalFeedLoader(store: store)
         XCTAssertEqual(store.deleteCachedFeedCallCount, 0)
     }
+    
+    func test_save_requestCacheDeletion() {
+        let store = FeedStore()
+        let sut = LocalFeedLoader(store: store)
+        
+        let items = [uniqueItem() , uniqueItem()]
+        sut.save(items)
+        
+        XCTAssertEqual(store.deleteCachedFeedCallCount, 1)
+    }
 
+    private func uniqueItem() -> FeedItem {
+        return FeedItem(id: UUID(), imageURL: anyURL(), description: "any", location: "any")
+    }
+    
+    private func anyURL() -> URL {
+        
+        return URL(string: "https://any-url.com")!
+    }
+     
 }
